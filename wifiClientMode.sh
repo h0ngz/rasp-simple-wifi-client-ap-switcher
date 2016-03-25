77bcf6d14d62c78b520251e6bfab520edec33f86
h0ngz <h0ngz@naver.com>
2016-03-25 13:55:38 +0900

    Initialize Project

--- wifiClientMode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-/etc/init.d/hostapd stop 2>&1 >/dev/null
-/etc/init.d/udhcpd stop 2>&1 >/dev/null
-ifdown wlan0 2>&1 >/dev/null
-cp /etc/network/interfaces.client /etc/network/interfaces
-ifup wlan0 2>&1 >/dev/null
